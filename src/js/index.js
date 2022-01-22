320f6b93bee0ce2aae953606586faad56d379d09
Vi <engrycat@gmail.com>
2022-01-22 18:07:27 +0200

    add new files

--- src/js/index.js
+++ src/js/index.js
@@ -1,243 +1,450 @@
-let question = [
-  "Какой знак восточного гороскопа следует за знаком Дракона?",
-  "В какую одежду принято плакать, чтобы вызвать сочувствие?",
-  "Какое из этих украшений можно встретить на новогодней ёлке?",
-  "Какой цвет получается при смешении синего и красного?",
-  "В какой стране обитают кенгуру?",
-  "В состав любого органического вещества входит…",
-  "Разновидностью какого минерала является горный хрусталь?",
-  "Что кричат болельщики на хоккейном матче?",
-  "Кто изобрел громоотвод?",
-  "	Какие семьи, по мнению Л. Н. Толстого, похожи друг на друга?",
-  "В каком городе находится штаб-квартира Microsoft?",
-  "Как называют период времени, когда солнце в северных широтах не опускается за горизонт?",
-  "Ричард Львиное Сердце был пленен во время",
-  "Какое астрономическое явление жители Земли могут наблюдать один раз в 76 лет?",
-  "Кто такой «молотоглав»?",
+const questionNode = document.querySelector(".question");
+const answersNode = document.querySelector(".answer");
+const timerNode = document.querySelector("#timer_inp");
+
+const finishButton = document.querySelector(".helps_game-end");
+finishButton.addEventListener("click", () => {
+  if (!finishButton.classList.contains("disabled")) {
+    alert(
+      `Игра завершена! Вы выиграли: ${
+        document.querySelector(".active").previousSibling.textContent
+      } ничего 🙂`
+    );
+    location.reload();
+  }
+});
+
+let gameData = [
+  {
+    question: "Какой знак восточного гороскопа следует за знаком Дракона?",
+    answers: [
+      {
+        answer: "Кот Кролик",
+        correct: false,
+      },
+      {
+        answer: "Змея",
+        correct: true,
+      },
+      {
+        answer: "Собака",
+        correct: false,
+      },
+      {
+        answer: "Крыса",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "В какую одежду принято плакать, чтобы вызвать сочувствие?",
+    answers: [
+      {
+        answer: "В рубашку",
+        correct: false,
+      },
+      {
+        answer: "В жилетку",
+        correct: true,
+      },
+      {
+        answer: "В платье",
+        correct: false,
+      },
+      {
+        answer: "В кутрку",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Какое из этих украшений можно встретить на новогодней ёлке?",
+    answers: [
+      {
+        answer: "Перстень",
+        correct: false,
+      },
+      {
+        answer: "Серьги",
+        correct: false,
+      },
+      {
+        answer: "Бусы",
+        correct: true,
+      },
+      {
+        answer: "Браслет",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Какой цвет получается при смешении синего и красного?",
+    answers: [
+      {
+        answer: "Коричневый",
+        correct: false,
+      },
+      {
+        answer: "Фиолетовый",
+        correct: true,
+      },
+      {
+        answer: "Зеленый",
+        correct: false,
+      },
+      {
+        answer: "Голубой",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "В какой стране обитают кенгуру?",
+    answers: [
+      {
+        answer: "Гаити",
+        correct: false,
+      },
+      {
+        answer: "Португалия",
+        correct: false,
+      },
+      {
+        answer: "Россия",
+        correct: false,
+      },
+      {
+        answer: "Австралия",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "В состав любого органического вещества входит…",
+    answers: [
+      {
+        answer: "Кислород",
+        correct: false,
+      },
+      {
+        answer: "Углерод",
+        correct: true,
+      },
+      {
+        answer: "Водород",
+        correct: false,
+      },
+      {
+        answer: "Азот",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Разновидностью какого минерала является горный хрусталь?",
+    answers: [
+      {
+        answer: "Опал",
+        correct: false,
+      },
+      {
+        answer: "Кварца",
+        correct: true,
+      },
+      {
+        answer: "Графит",
+        correct: false,
+      },
+      {
+        answer: "Алмаз",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Что кричат болельщики на хоккейном матче?",
+    answers: [
+      {
+        answer: "Гайку!",
+        correct: false,
+      },
+      {
+        answer: "Вперед!",
+        correct: false,
+      },
+      {
+        answer: "Шайбу!",
+        correct: true,
+      },
+      {
+        answer: "Ракету!",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Кто изобрел громоотвод?",
+    answers: [
+      {
+        answer: "Франклин",
+        correct: true,
+      },
+      {
+        answer: "Рузвельт",
+        correct: false,
+      },
+      {
+        answer: "Вашингтон",
+        correct: false,
+      },
+      {
+        answer: "Линкольн",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Какие семьи, по мнению Л. Н. Толстого, похожи друг на друга?",
+    answers: [
+      {
+        answer: "Дворовые",
+        correct: false,
+      },
+      {
+        answer: "Грустые",
+        correct: false,
+      },
+      {
+        answer: "Счастливые",
+        correct: true,
+      },
+      {
+        answer: "Известные",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "В каком городе находится штаб-квартира Microsoft?",
+    answers: [
+      {
+        answer: "Нью-Йорк",
+        correct: false,
+      },
+      {
+        answer: "Редмонд",
+        correct: true,
+      },
+      {
+        answer: "Новый Орлеан",
+        correct: false,
+      },
+      {
+        answer: "Сиэтл",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question:
+      "Как называют период времени, когда солнце в северных широтах не опускается за горизонт?",
+    answers: [
+      {
+        answer: "Великий день",
+        correct: false,
+      },
+      {
+        answer: "Солнцестояние",
+        correct: false,
+      },
+      {
+        answer: "Солнечное затмение",
+        correct: false,
+      },
+      {
+        answer: "Полярный день",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Ричард Львиное Сердце был пленен во время",
+    answers: [
+      {
+        answer: "Крестового похода",
+        correct: true,
+      },
+      {
+        answer: "Столетней войны",
+        correct: false,
+      },
+      {
+        answer: "Войны алой и белой розы",
+        correct: false,
+      },
+      {
+        answer: "Войны за независимость",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question:
+      "Какое астрономическое явление жители Земли могут наблюдать один раз в 76 лет?",
+    answers: [
+      {
+        answer: "Звездопад",
+        correct: false,
+      },
+      {
+        answer: "Астероид",
+        correct: false,
+      },
+      {
+        answer: "Комета Галлея",
+        correct: true,
+      },
+      {
+        answer: "Самолет",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Кто такой «молотоглав»?",
+    answers: [
+      {
+        answer: "Рыба",
+        correct: false,
+      },
+      {
+        answer: "Птица",
+        correct: true,
+      },
+      {
+        answer: "Змея",
+        correct: false,
+      },
+      {
+        answer: "Насекомое",
+        correct: false,
+      },
+    ],
+  },
 ];
-let answer = [
-  "Кот Кролик",
-  "Змея",
-  "Собака",
-  "Крыса",
-  "В рубашку",
-  "В жилетку",
-  "В платье",
-  "В кутрку",
-  "Гирлянда",
-  "Серьги",
-  "Бусы",
-  "Браслет",
-  "Коричневый",
-  "Фиолетовый",
-  "Зеленый",
-  "Голубой",
-  "Гаити",
-  "Португалия",
-  "Россия",
-  "Австралия",
-  "Кислород",
-  "Углерод",
-  "Водород",
-  "Азот",
-  "Опал",
-  "Кварца ",
-  "Графит",
-  "Алмаз",
-  "Гайку!",
-  "Вперед!",
-  "Шайбу!",
-  "Ракету!",
-  "Франклин",
-  "Рузвельт",
-  "Вашингтон",
-  "Линкольн",
-  "Дворовые",
-  "Грустые",
-  "Счастливые",
-  "Известные",
-  "Нью-Йорк",
-  "Редмонд",
-  "Новый Орлеан",
-  "Сиэтл",
-  "Великий день",
-  "Солнцестояние",
-  "Солнечное затмение",
-  "Полярный день",
-  "Крестового похода",
-  "Столетней войны",
-  "Войны алой и белой розы",
-  "Войны за независимость",
-  "Звезда",
-  "Астероид",
-  "Комета Галлея",
-  "Самолет",
-  "Рыба",
-  "Птица",
-  "Змея",
-  "Насекомое",
-];
-let key = [1, 3, 3, 2, 3, 1, 3, 2, 0, 1, 1, 3, 0, 1, 1];
+let sum = 0;
 
-let level = 0;
+game();
+async function game() {
+  let win = true;
+  for (let index = 0; index < gameData.length; index++) {
+    if (index !== 0) {
+      let currentLevel = document.querySelector(".active");
+      currentLevel.classList.remove("active");
+      currentLevel.nextElementSibling.classList.add("active");
+      if (currentLevel.classList.contains("no-fear")) {
+        sum = Number(currentLevel.textContent.replace(" ", ""));
+      }
+    }
+    if (index === 1) {
+      finishButton.classList.remove("disabled");
+    }
 
-let name = "name";
-let username = readCookie(name);
+    const currentGameData = gameData[index];
+    questionNode.textContent = currentGameData.question;
+    for (let index = 0; index < currentGameData.answers.length; index++) {
+      let answerNode = answersNode.children[index];
+      if (answerNode.classList.contains("helping")) {
+        answerNode.classList.remove("helping");
+      }
 
-if (username != null) {
-  $(".start").css("display", "none");
-  $(".reStart").css("display", "block");
-  $(".hellow").text("С возвращением, " + username + "!");
+      const answerData = currentGameData.answers[index];
+      answerNode.textContent = answerData.answer;
+      answerNode.setAttribute("correct", answerData.correct);
+    }
 
-  $("#startGame").click(function () {
-    $(".reStart").css("display", "none");
-    setTimeout(timer, 1000);
+    // вызываем функцию и ждем ответ
+    let checkState = await waitAnswer();
+
+    if (checkState === false) {
+      win = false;
+      break;
+    }
+  }
+
+  if (win === true) {
+    alert(
+      `Игра завершена! Вы выиграли: ${answersNode.lastChild.textContent} ничего 🙂`
+    );
+  } else {
+    alert(`Игра завершена! Вы выиграли: ${sum} ничего 🙂`);
+  }
+  location.reload();
+}
+
+/**
+ * Возвращает true когда был выбран правильный ответ или false когда ответ не правильный.
+ * @returns {boolean}
+ */
+function waitAnswer() {
+  return new Promise((resolve, reject) => {
+    let timeOut = 60;
+    timerNode.textContent = timeOut;
+    let timerId = setInterval(() => {
+      timeOut--;
+      timerNode.textContent = timeOut;
+      if (timeOut === 0) {
+        clearInterval(timerId);
+        return resolve(false);
+      }
+    }, 1000);
+
+    for (let index = 0; index < 4; index++) {
+      const buttonNode = answersNode.children[index];
+      buttonNode.addEventListener("click", (event) => {
+        if (event.target.getAttribute("correct") === "true") {
+          clearInterval(timerId);
+          return resolve(true);
+        } else {
+          clearInterval(timerId);
+          return resolve(false);
+        }
+      });
+    }
   });
 }
 
-function show(level) {
-  $(".question").text(question[level]);
-  $("label[for=answer1]").text(answer[level * 4 + 0]);
-  $("label[for=answer2]").text(answer[level * 4 + 1]);
-  $("label[for=answer3]").text(answer[level * 4 + 2]);
-  $("label[for=answer4]").text(answer[level * 4 + 3]);
-}
+document.querySelector(".helps_call").addEventListener("click", friendCall);
+let usedFriendCall = false;
+function friendCall() {
+  if (usedFriendCall) {
+    return true;
+  }
+  usedFriendCall = true;
 
-let resultConst = [];
-show(level);
-let tr = $("tr");
-$(tr[tr.length - (level + 1)]).css("background", "#FF0");
-
-$(".btn").click(function () {
-  $("#timer_inp").text(60);
-
-  if ($("input[name=answer]:checked").val() == key[level]) {
-    level++;
-    show(level);
+  let friendTrue = Math.round(Math.random());
+  let randomField = Math.floor(Math.random() * 3);
+  if (friendTrue === 1) {
+    answersNode.querySelector('[correct="true"]').classList.add("helping");
   } else {
-    gameOwer();
-  }
-
-  $("input").prop("checked", false);
-  $(tr.css("background", "#fff"));
-  $(tr.removeClass("result"));
-  $(tr[tr.length - (level + 1)]).css("background", "#FF0");
-  $(tr[tr.length - level]).css("color", "#f0f");
-  $(tr[tr.length - level]).addClass("result");
-  $("label").css("color", "#555");
-
-  if (level == 5 || level == 10 || level == 15) {
-    resultConst.push($(tr[tr.length - level]).addClass("resultConst"));
-  }
-});
-
-Math.rand = function (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-};
-
-let inputLabel = document.getElementsByTagName("label");
-$(".round50").click(function () {
-  let inputAnswer = document.getElementsByName("answer");
-  let exp = [];
-  let count = 0;
-  while (count < 2) {
-    let index = Math.rand(0, 3);
-    if (exp.indexOf(index) == -1 && $(inputAnswer[index]).val() != key[level]) {
-      $(inputLabel[index]).css("color", "#69f");
-      count++;
-      exp.push(index);
-    }
-  }
-  $(this).off("click");
-  $(this).css("background", "red");
-});
-
-$(".round").click(function () {
-  $(inputLabel[Math.rand(0, 3)]).css("color", "#F90"), $(this).off("click");
-  $(this).css("background", "red");
-});
-
-let result = $(".result");
-$(".roundEnd").click(function () {
-  end();
-});
-
-function end() {
-  $(".end").css("display", "block");
-
-  if (tr.hasClass("result")) {
-    let tdResult = $("tr.result").children();
-    let tdText = tdResult[1].textContent;
-    $(".showResult").text("ВЫ ВЫИГРАЛИ: " + tdText + " гривень");
+    answersNode
+      .querySelectorAll('[correct="false"]')
+      [randomField].classList.add("helping");
   }
 }
 
-function gameOwer() {
-  $(".end").css("display", "block");
-
-  if (tr.hasClass("resultConst")) {
-    let tdResult1 = $(resultConst[resultConst.length - 1]).children();
-    let tdText1 = tdResult1[1].textContent;
-    $(".showResult").text("ВЫ ВЫИГРАЛИ: " + tdText1 + " гривень");
+document.querySelector(".helps_fifty").addEventListener("click", fifty);
+let usedfifty = false;
+function fifty() {
+  if (usedfifty) {
+    return true;
   }
-}
+  usedfifty = true;
 
-function timer() {
-  let objTimer = document.getElementById("timer_inp");
-  objTimer.innerHTML--;
-
-  if (objTimer.innerHTML == 5) {
-    $("#timer_inp").css("background", "red");
-  }
-  if (objTimer.innerHTML == 0) {
-    setTimeout(function () {}, 1000);
-    gameOwer();
-  } else {
-    setTimeout(timer, 1000);
-  }
-}
-
-$("form").submit(function (e) {
-  e.preventDefault();
-});
-
-$("#start").click(function () {
-  if ($("#user").val() != "") {
-    $(".start").css("display", "none");
-    setTimeout(timer, 1000);
-  } else {
-    $("#user").css("background", "#f30");
-  }
-
-  let value = $("#user").val();
-
-  createCookie(name, value, 1);
-});
-
-function createCookie(name, value, days) {
-  if (days) {
-    let date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    let expires = "; expires=" + date.toGMTString();
-  } else {
-    let expires = "";
-  }
-  document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-  let nameEQ = name + "=";
-  let ca = document.cookie.split(";");
-
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(nameEQ) == 0) {
-      let value = c.substring(nameEQ.length, c.length);
-      return value.split(",");
-    }
-  }
-  return null;
+  let randomField = Math.floor(Math.random() * 3);
+  answersNode.querySelector('[correct="true"]').classList.add("helping");
+  answersNode
+    .querySelectorAll('[correct="false"]')
+    [randomField].classList.add("helping");
 }
